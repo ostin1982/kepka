85a13610e9f06b7dcfbfc34bc4e4e6f647c2eaf5
Denis Sokolov <ostin1982@yandex.ru>
2023-03-21 09:39:24 +0300

    feat: layout of the Kepka landing page

---
+++ script/script.js
@@ -0,0 +1,260 @@
+const politicsText = `
+    <p class="title">Политика конфиденциальности</p>
+    <div class="module">
+        <p class="text">Политика в отношении обработки персональных данных</p>
+        <p class="text"><b>1. Общие положения</b></p>
+        <p class="text">Настоящая политика обработки персональных данных составлена в соответствии с требованиями Федерального закона от 27.07.2006. №152-ФЗ «О персональных данных» и определяет порядок обработки персональных данных и меры по обеспечению безопасности персональных данных Интернет сайт  (далее – Оператор).</p>
+        <p class="text"> Оператор ставит своей важнейшей целью и условием осуществления своей деятельности соблюдение прав и свобод человека и гражданина при обработке его персональных данных, в том числе защиты прав на неприкосновенность частной жизни, личную и семейную тайну. </p>
+        <p class="text"> Настоящая политика Оператора в отношении обработки персональных данных (далее – Политика) применяется ко всей информации, которую Оператор может получить о посетителях веб-сайта . </p>
+        <p class="text"><b>2. Основные понятия, используемые в Политике</b></p>
+        <p class="text"> Автоматизированная обработка персональных данных – обработка персональных данных с помощью средств вычислительной техники; </p>
+        <p class="text"> Блокирование персональных данных – временное прекращение обработки персональных данных (за исключением случаев, если обработка необходима для уточнения персональных данных); <p>
+        <p class="text"> Веб-сайт – совокупность графических и информационных материалов, а также программ для ЭВМ и баз данных, обеспечивающих их доступность в сети интернет по сетевому адресу ; <p>
+        <p class="text"> Информационная система персональных данных — совокупность содержащихся в базах данных персональных данных, и обеспечивающих их обработку информационных технологий и технических средств; </li>
+        <p class="text"> Любая информация на сайте не является публичной офертой. <p>
+        <p class="text"> Бесплатная диагностика, является предложением по акции и действует только при продолжении ремонта. В случае отказа от ремонта Заказчик оплачивает стоимость диагностики в размере от пятисот до двух тысяч рублей. Стоимость диагностики определяется инженером после ее проведения. <p>
+        <p class="text"> Обезличивание персональных данных — действия, в результате которых невозможно определить без использования дополнительной информации принадлежность персональных данных конкретному Пользователю или иному субъекту персональных данных; <p>
+        <p class="text"> Обработка персональных данных – любое действие (операция) или совокупность действий (операций), совершаемых с использованием средств автоматизации или без использования таких средств с персональными данными, включая сбор, запись, систематизацию, накопление, хранение, уточнение (обновление, изменение), извлечение, использование, передачу (распространение, предоставление, доступ), обезличивание, блокирование, удаление, уничтожение персональных данных; <p>
+        <p class="text"> Оператор – государственный орган, муниципальный орган, юридическое или физическое лицо, самостоятельно или совместно с другими лицами организующие и (или) осуществляющие обработку персональных данных, а также определяющие цели обработки персональных данных, состав персональных данных, подлежащих обработке, действия (операции), совершаемые с персональными данными; <p>
+        <p class="text"> Персональные данные – любая информация, относящаяся прямо или косвенно к определенному или определяемому Пользователю веб-сайта  <p>
+        <p class="text"> Пользователь – любой посетитель веб-сайта ; <p>
+        <p class="text"> Предоставление персональных данных – действия, направленные на раскрытие персональных данных определенному лицу или определенному кругу лиц; <p>
+        <p class="text"> Распространение персональных данных – любые действия, направленные на раскрытие персональных данных неопределенному кругу лиц (передача персональных данных) или на ознакомление с персональными данными неограниченного круга лиц, в том числе обнародование персональных данных в средствах массовой информации, размещение в информационно-телекоммуникационных сетях или предоставление доступа к персональным данным каким-либо иным способом; </p>
+        <p class="text"> Трансграничная передача персональных данных – передача персональных данных на территорию иностранного государства органу власти иностранного государства, иностранному физическому или иностранному юридическому лицу; </p>
+        <p class="text"> Уничтожение персональных данных – любые действия, в результате которых персональные данные уничтожаются безвозвратно с невозможностью дальнейшего восстановления содержания персональных данных в информационной системе персональных данных и (или) результате которых уничтожаются материальные носители персональных данных. </p>
+        <p class="text"><b>3. Оператор может обрабатывать следующие персональные данные Пользователя</b></p>
+        <p class="text"> Год, месяц, дата и место рождения; </p>
+        <p class="text"> Фотографии; </p>
+        <p class="text"> Также на сайте происходит сбор и обработка обезличенных данных о посетителях (в т.ч. файлов «cookie») с помощью сервисов интернет-статистики (Яндекс Метрика и Гугл Аналитика и других). </p>
+        <p class="text"> Вышеперечисленные данные далее по тексту Политики объединены общим понятием Персональные данные. </p>
+        <p class="text"><b>4. Цели обработки персональных данных</b></p> 
+        <p class="text"> Цель обработки персональных данных Пользователя — информирование Пользователя посредством отправки электронных писем; предоставление доступа Пользователю к сервисам, информации и/или материалам, содержащимся на веб-сайте. </p>
+        <p class="text"> Также Оператор имеет право направлять Пользователю уведомления о новых продуктах и услугах, специальных предложениях и различных событиях. Пользователь всегда может отказаться от получения информационных сообщений, направив Оператору письмо на адрес электронной почты info@ с пометкой «Отказ от уведомлениях о новых продуктах и услугах и специальных предложениях». </p>
+        <p class="text"> Обезличенные данные Пользователей, собираемые с помощью сервисов интернет-статистики, служат для сбора информации о действиях Пользователей на сайте, улучшения качества сайта и его содержания. </p>
+        <p class="text"><b>5. Правовые основания обработки персональных данных</b></p>
+        <p class="text"> Оператор обрабатывает персональные данные Пользователя только в случае их заполнения и/или отправки Пользователем самостоятельно через специальные формы, расположенные на сайте . Заполняя соответствующие формы и/или отправляя свои персональные данные Оператору, Пользователь выражает свое согласие с данной Политикой. </p>
+        <p class="text"> Оператор обрабатывает обезличенные данные о Пользователе в случае, если это разрешено в настройках браузера Пользователя (включено сохранение файлов «cookie» и использование технологии JavaScript). </p>
+        <p class="text"><b>6. Порядок сбора, хранения, передачи и других видов обработки персональных данных</b></p>
+        <p class="text">Безопасность персональных данных, которые обрабатываются Оператором, обеспечивается путем реализации правовых, организационных и технических мер, необходимых для выполнения в полном объеме требований действующего законодательства в области защиты персональных данных.</p>
+        <p class="text"> Оператор обеспечивает сохранность персональных данных и принимает все возможные меры, исключающие доступ к персональным данным неуполномоченных лиц. </p>
+        <p class="text"> Персональные данные Пользователя никогда, ни при каких условиях не будут переданы третьим лицам, за исключением случаев, связанных с исполнением действующего законодательства. </p>
+        <p class="text"> В случае выявления неточностей в персональных данных, Пользователь может актуализировать их самостоятельно, путем направления Оператору уведомление на адрес электронной почты Оператора info@ с пометкой «Актуализация персональных данных». </p>
+        <p class="text"> Срок обработки персональных данных является неограниченным. Пользователь может в любой момент отозвать свое согласие на обработку персональных данных, направив Оператору уведомление посредством электронной почты на электронный адрес Оператора info@ с пометкой «Отзыв согласия на обработку персональных данных». </p>
+        <p class="text"><b>7. Трансграничная передача персональных данных</b></p> 
+        <p class="text"> Оператор до начала осуществления трансграничной передачи персональных данных обязан убедиться в том, что иностранным государством, на территорию которого предполагается осуществлять передачу персональных данных, обеспечивается надежная защита прав субъектов персональных данных. </p>
+        <p class="text"> Трансграничная передача персональных данных на территории иностранных государств, не отвечающих вышеуказанным требованиям, может осуществляться только в случае наличия согласия в письменной форме субъекта персональных данных на трансграничную передачу его персональных данных и/или исполнения договора, стороной которого является субъект персональных данных. </p>
+        <p class="text"><b>8. Заключительные положения</b></p>
+        <p class="text"> Пользователь может получить любые разъяснения по интересующим вопросам, касающимся обработки его персональных данных, обратившись к Оператору с помощью электронной почты info@. </p>
+        <p class="text"> В данном документе будут отражены любые изменения политики обработки персональных данных Оператором. Политика действует бессрочно до замены ее новой версией. </p>
+    </div>
+`
+const body = document.querySelector('body');
+const closeModals = body.querySelectorAll('.js-modal-close');
+const openModalForm = body.querySelectorAll('.js-call');
+const modalForm = body.querySelector('.js-modal-form');
+const openModalPolitics = body.querySelectorAll('.js-politics');
+const modalPolitics = body.querySelector('.js-modal-politics');
+const upload = body.querySelector('.upload');
+const pricingLinks = body.querySelectorAll('.pricing .link');
+const pricingBlocks = body.querySelectorAll('.pricing .block');
+const accordionCards = body.querySelectorAll('.faq .card');
+const accordionQuestion = body.querySelectorAll('.faq .question');
+const phone = body.querySelectorAll('.js-phone');
+
+const scrollWidth = window.innerWidth - body.offsetWidth + 'px';
+
+const openModalWindow = (open, window) => {
+    open.forEach(press => {
+        press.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            window.classList.add('open');
+            body.classList.add('lock');
+            body.style.paddingRight = scrollWidth;
+        })
+    });
+};
+
+const closeModalWindow = (close, window) => {
+    close.forEach(press => {
+        press.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            window.classList.remove('open');
+            body.classList.remove('lock');
+            body.style.paddingRight = '0px'
+        })
+    })
+};
+
+const closeModalWindowOutOfField = (window) => {
+    document.addEventListener('mousedown', (event) => { 
+        if (event.target === window) {
+            event.preventDefault();
+            
+            window.classList.remove('open');
+            body.classList.remove('lock');
+            body.style.paddingRight = '0px'
+        }
+    });
+};
+
+openModalWindow(openModalForm, modalForm);
+closeModalWindow(closeModals, modalForm);
+closeModalWindowOutOfField(modalForm);
+
+openModalWindow(openModalPolitics, modalPolitics);
+closeModalWindow(closeModals, modalPolitics);
+closeModalWindowOutOfField(modalPolitics); 
+
+openModalPolitics.forEach(openModalPolitic => {
+    openModalPolitic.addEventListener('click', () => {
+        upload.insertAdjacentHTML('afterbegin', politicsText);
+    })
+});
+
+closeModals.forEach(closeModal => {
+    closeModal.addEventListener('click', () => {
+        upload.innerHTML = ''
+    })
+});
+
+accordionQuestion.forEach(accordionCard => {    
+    accordionCard.addEventListener('click', () => {
+        const parentAccordionCard = accordionCard.parentNode;
+        
+        if(parentAccordionCard.classList.contains('open')) {
+            parentAccordionCard.classList.remove('open')
+        } else {
+            accordionCards.forEach(accordionCard => {
+                accordionCard.classList.remove('open');
+                parentAccordionCard.classList.add('open')
+            })
+        }
+    })
+})
+
+pricingLinks.forEach(pricingLink => {
+    pricingLink.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const id = event.target.getAttribute('href').replace('#', '');
+
+        pricingLinks.forEach(childLink => {
+            childLink.classList.remove('active');
+        })
+
+        pricingBlocks.forEach(childBlock => {
+            childBlock.classList.remove('active');
+        })
+
+        pricingLink.classList.add('active');
+        document.getElementById(id).classList.add('active');
+    })
+})
+
+const burgerMenu = body.querySelector('.header .menu');
+const headerMenuNav = body.querySelector('header .nav');
+const links = body.querySelectorAll('.js-link');
+let burgerMenuOpen = false;
+
+burgerMenu.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if(!burgerMenuOpen) {
+        burgerMenu.classList.add('active');
+        headerMenuNav.classList.add('active');
+        body.classList.add('lock');
+        burgerMenuOpen = true;
+    } else {
+        burgerMenu.classList.remove('active');
+        headerMenuNav.classList.remove('active');
+        body.classList.remove('lock');
+        burgerMenuOpen = false;
+    }
+
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+
+            burgerMenu.classList.remove('active');
+            headerMenuNav.classList.remove('active');
+            body.classList.remove('lock');
+            burgerMenuOpen = false;
+        })
+    })
+    
+})
+
+window.addEventListener("DOMContentLoaded", () => {
+    [].forEach.call(phone, (input) => {
+    let keyCode;
+    function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        const pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        let matrix = "+7 (___) ___-__-__",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            new_value = matrix.replace(/[_\d]/g, (a) => {
+                return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+            });
+        i = new_value.indexOf("_");
+        if (i != -1) {
+            i < 5 && (i = 3);
+            new_value = new_value.slice(0, i)
+        }
+        let reg = matrix.substring(0, this.value.length).replace(/_+/g,
+            (a) => {
+                return "\\d{1," + a.length + "}"
+            }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+        if (event.type == "blur" && this.value.length < 5)  this.value = ""
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false)
+    });
+});
+
+
+// scrolling animation
+const animationItems = body.querySelectorAll('.js-animation');
+
+if(animationItems.length > 0) {
+    const offset = (element) => {
+        const rect = element.getBoundingClientRect();
+        const scrollLeft = window.scrollX || document.documentElement.scrollLeft;
+        const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    const animeOnScroll = () => {
+        animationItems.forEach(animationItem => {
+            const animationItemHeight = animationItem.offsetHeight;
+            const animationItemOffset = offset(animationItem).top;
+            const animationStart = 6;
+
+            let animationItemPoint = window.innerHeight - animationItemHeight / animationStart;
+
+            if(animationItemHeight > window.innerHeight) {
+                animationItemPoint = window.innerHeight - window.innerHeight / animationStart;
+            }
+
+            if(scrollY > (animationItemOffset - animationItemPoint) && scrollY < (animationItemOffset + animationItemHeight)) {
+                animationItem.classList.add('active')
+            } else {
+                if(!animationItem.classList.contains('js-not-animation')) {
+                    animationItem.classList.remove('active')
+                }
+            }
+        })
+    }
+
+    animeOnScroll();
+    window.addEventListener('scroll', animeOnScroll)
+}
